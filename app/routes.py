74e9cc5351f9d9dc78402717e9a41e7f08227155
Albert <pylame22@yandex.ru>
2022-11-25 18:26:04 +0300

    review

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,14 @@
 from fastapi import APIRouter
 
 from app.services.boards import get_boards_service
+from boards.routes import router as board_router
 
 router = APIRouter()
 
+router.api_route(board_router)
+router.api_route(board_router)
+router.api_route(board_router)
+router.api_route(board_router)
 
 @router.get("/boards")
 def get_all_boards_api():
